3bf6d4548d880a3c33c0f91c73b998118f29cbf9
wzz <404464384@qq.com>
2021-09-12 17:24:02 +0800

    1

--- clothes_test.py
+++ clothes_test.py
@@ -12,87 +12,91 @@ from mnist import MNIST
 from PIL import Image
 import os
 import cv2
+for a2 in range(1):
+    a2=80
+    # data = scipy.io.loadmat('clothes5.mat') # 读取mat文件
+    data = scipy.io.loadmat('T4.mat') # 读取mat文件
+    # print(data.keys())  # 查看mat文件中的所有变量
+    image_vecs=data['test_data']
+    image_vecs=preprocessing.normalize(image_vecs.T, norm='l2').T
+    image_vecs=norm_Ys(image_vecs)
+    labels_mat=data['test_Annotation']
+    # image_vecs=data['train_data']
+    # image_vecs=preprocessing.normalize(image_vecs.T, norm='l2').T
+    # labels_mat=data['train_Annotation']
+    labels_index=np.empty((labels_mat.shape[0],labels_mat.shape[1]))
+    labels_index[:]=-1
+    images_count=np.empty((5),dtype=int)
+    for i in range(labels_mat.shape[0]):
+        one_label_mat=labels_mat[i]
+        one_labels_index=np.where(one_label_mat==1)[0]
+        labels_index[i,:one_labels_index.shape[0]]=one_labels_index
+        images_count[i]=one_labels_index.shape[0]
 
-# data = scipy.io.loadmat('clothes5.mat') # 读取mat文件
-data = scipy.io.loadmat('T4.mat') # 读取mat文件
-# print(data.keys())  # 查看mat文件中的所有变量
-image_vecs=data['test_data']
-image_vecs=preprocessing.normalize(image_vecs.T, norm='l2').T
-image_vecs=norm_Ys(image_vecs)
-labels_mat=data['test_Annotation']
-# image_vecs=data['train_data']
-# image_vecs=preprocessing.normalize(image_vecs.T, norm='l2').T
-# labels_mat=data['train_Annotation']
-labels_index=np.empty((labels_mat.shape[0],labels_mat.shape[1]))
-labels_index[:]=-1
-images_count=np.empty((5),dtype=int)
-for i in range(labels_mat.shape[0]):
-    one_label_mat=labels_mat[i]
-    one_labels_index=np.where(one_label_mat==1)[0]
-    labels_index[i,:one_labels_index.shape[0]]=one_labels_index
-    images_count[i]=one_labels_index.shape[0]
+    t=time.time()
 
-t=time.time()
+    np.random.seed(int(t)%100)
+    n_classes=labels_index.shape[0]
+    classes=np.arange(n_classes)
+    # ind_to_lab_dir={0:"仫佬族",1:"纳西族",2:"怒族",3:"普米族",4:"羌族",5:"撒拉族",6:"畲族"}
+    lab_to_ind_dir={0:0,1:1,2:2,3:3,4:4}
+    ind_to_lab_dir={0:0,1:1,2:2,3:3,4:4}
+    w=54
+    h=46
 
-np.random.seed(int(t)%100)
-n_classes=labels_index.shape[0]
-classes=np.arange(n_classes)
-# ind_to_lab_dir={0:"仫佬族",1:"纳西族",2:"怒族",3:"普米族",4:"羌族",5:"撒拉族",6:"畲族"}
-lab_to_ind_dir={0:0,1:1,2:2,3:3,4:4}
-ind_to_lab_dir={0:0,1:1,2:2,3:3,4:4}
-w=54
-h=46
+    py_file_name="clothes"
 
-py_file_name="clothes"
+    start_init_number=30
+    train_number=300
+    update_times=1000
+    im_vec_len=w*h
+    n_atoms = start_init_number
+    n_neighbor = 8
+    lamda = 0.5
+    beta = 1.
+    gamma = 1.
+    mu = 2.*gamma
+    r = 2.
+    c = 1.
 
-start_init_number=30
-train_number=300
-update_times=100
-im_vec_len=w*h
-n_atoms = start_init_number
-n_neighbor = 8
-lamda = 0.5
-beta = 1.
-gamma = 1.
-mu = 2.*gamma
-r = 2.
-c = 1.
+    seed = 0 # to save the way initialize dictionary
+    n_iter_sp = 50 #number max of iteration in sparse coding
+    n_iter_du = 50 # number max of iteration in dictionary update
+    n_iter = 15 # number max of general iteration
+    transform_n_nonzero_coefs=30
+    n_features = image_vecs.shape[0]
 
-seed = 0 # to save the way initialize dictionary
-n_iter_sp = 50 #number max of iteration in sparse coding
-n_iter_du = 50 # number max of iteration in dictionary update
-n_iter = 15 # number max of general iteration
-transform_n_nonzero_coefs=30
-n_features = image_vecs.shape[0]
+    # inds_of_file_path_path='inds_of_file_path_wzz_'+py_file_name+'_'+str(w)+'_'+str(h)+'_'+str(update_times)+'_'+str(transform_n_nonzero_coefs)+'_'+str(start_init_number)+'.npy'
+    # inds_of_file_path=np.load(inds_of_file_path_path)
 
-# inds_of_file_path_path='inds_of_file_path_wzz_'+py_file_name+'_'+str(w)+'_'+str(h)+'_'+str(update_times)+'_'+str(transform_n_nonzero_coefs)+'_'+str(start_init_number)+'.npy'
-# inds_of_file_path=np.load(inds_of_file_path_path)
+    D_all=np.load("D_all_"+py_file_name+"_mulD_"+str(w)+"_"+str(h)+'_'+str(transform_n_nonzero_coefs)+'_'+str(start_init_number)+"_"+str(train_number)+"_"+str(update_times)+"_"+str(a2)+".npy")
+    W_all=np.load("W_all_"+py_file_name+"_mulD_"+str(w)+"_"+str(h)+'_'+str(transform_n_nonzero_coefs)+'_'+str(start_init_number)+"_"+str(train_number)+"_"+str(update_times)+"_"+str(a2)+".npy")
 
-D_all=np.load("D_all_"+py_file_name+"_mulD_"+str(w)+"_"+str(h)+'_'+str(transform_n_nonzero_coefs)+'_'+str(start_init_number)+"_"+str(train_number)+"_"+str(update_times)+".npy")
-W_all=np.load("W_all_"+py_file_name+"_mulD_"+str(w)+"_"+str(h)+'_'+str(transform_n_nonzero_coefs)+'_'+str(start_init_number)+"_"+str(train_number)+"_"+str(update_times)+".npy")
+    average_accuracy=0.
 
-average_accuracy=0.
-
-Y_test=image_vecs
-test_number=Y_test.shape[1]
-X_test=np.empty((D_all.shape[1],test_number))
-coder = SparseCoder(dictionary=D_all.T,transform_n_nonzero_coefs=transform_n_nonzero_coefs, transform_algorithm="omp")
-X_test=(coder.transform(Y_test.T)).T
-# X_test=transform(D_all,Y_test,transform_n_nonzero_coefs)
-# for i in range(n_classes):
-#     D=D_all[:,i*n_atoms:(i+1)*n_atoms]
-#     coder = SparseCoder(dictionary=D.T,transform_n_nonzero_coefs=transform_n_nonzero_coefs, transform_algorithm="omp")
-#     X_test_part=(coder.transform(Y_test.T)).T
-#     X_test[i*n_atoms:(i+1)*n_atoms]=X_test_part
-the_H=np.dot(W_all,X_test)
-right_num=0
-for i in range(test_number):
-    pre=the_H[:,i].argmax()
-    if labels_mat[pre,i]==1:
-        right_num=right_num+1
-    else:
-        pass
-        # print(the_H[:,i])
-        # print(labels_mat[:,i])
-        # pdb.set_trace()
-print('accuracy : '+str(right_num*1./test_number))
+    Y_test=image_vecs
+    test_number=Y_test.shape[1]
+    X_test=np.empty((D_all.shape[1],test_number))
+    coder = SparseCoder(dictionary=D_all.T,transform_n_nonzero_coefs=transform_n_nonzero_coefs, transform_algorithm="omp")
+    X_test=(coder.transform(Y_test.T)).T
+    # X_test=transform(D_all,Y_test,transform_n_nonzero_coefs)
+    # for i in range(n_classes):
+    #     D=D_all[:,i*n_atoms:(i+1)*n_atoms]
+    #     coder = SparseCoder(dictionary=D.T,transform_n_nonzero_coefs=transform_n_nonzero_coefs, transform_algorithm="omp")
+    #     X_test_part=(coder.transform(Y_test.T)).T
+    #     X_test[i*n_atoms:(i+1)*n_atoms]=X_test_part
+    the_H=np.dot(W_all,X_test)
+    right_num=0
+    for i in range(test_number):
+        pre=the_H[:,i].argmax()
+        if labels_mat[pre,i]==1:
+            right_num=right_num+1
+        else:
+            pass
+            # print(the_H[:,i])
+            # print(labels_mat[:,i])
+            # pdb.set_trace()
+    print('iter : '+str(a2))
+    print('accuracy : '+str(right_num*1./test_number))
+    print()
+    sys.stdout.flush()
--- learning_incoherent_dictionary.py
+++ learning_incoherent_dictionary.py
@@ -19,15 +19,19 @@ import copy
 def incoherent(the_D,the_Y,the_X,nIter):
     iIter=0
     u0=0.7
-    current_u=2.
     gram=np.dot(the_D.T,the_D)
-    the_K_max=gram.max()
-    u0=(1-the_K_max)/2.+the_K_max
-    while iIter<=nIter and current_u>u0:
-        gram=np.dot(the_D.T,the_D)
+    for i in range(gram.shape[0]):
+        gram[i][i]=-gram[i][i]
+    current_u=gram.max()
+    for i in range(gram.shape[0]):
+        gram[i][i]=-gram[i][i]
+    u0=current_u-(1-current_u)/2.
+    print("u0:"+str(u0))
+    print()
+    print("current_u:"+str(current_u))
+    print()
+    while iIter<nIter and current_u>u0:
         the_K=copy.deepcopy(gram)
-        the_K_max=the_K.max()
-        # u0=(1-the_K_max)/2.+the_K_max
         the_K[the_K<-u0]=-u0
         the_K[the_K>u0]=u0
         for i in range(the_K.shape[0]):
@@ -39,25 +43,32 @@ def incoherent(the_D,the_Y,the_X,nIter):
         eigenvalues=np.diag(eigenvalues)
         eigenvalues[eigenvalues<0]=0
         part_D=np.dot(np.sqrt(eigenvalues),Q.T)
-        pdb.set_trace()
         for i in range(the_D.shape[0]):
             the_D[i,:]=part_D[i%part_D.shape[0],:]
         the_D=preprocessing.normalize(the_D.T, norm='l2').T
-        pdb.set_trace()
         the_C=np.dot(the_Y,np.dot(the_D,the_X).T)
-        the_W=the_u,the_s,the_vt=LA.svd(the_C)
-        np.dot(the_vt.T,the_u.T)
+        the_u,the_s,the_vt=LA.svd(the_C)
+        the_W=np.dot(the_vt.T,the_u.T)
         the_D=np.dot(the_W,the_D)
-        current_u=get_coherent(the_D)
-        print(current_u)
+        gram=np.dot(the_D.T,the_D)
+        for i in range(gram.shape[0]):
+            gram[i][i]=-gram[i][i]
+        current_u=gram.max()
+        for i in range(gram.shape[0]):
+            gram[i][i]=-gram[i][i]
+        print("u0:"+str(u0))
+        print()
+        print("current_u:"+str(current_u))
+        print()
         sys.stdout.flush()
+        iIter+=1
         # the_K=np.linalg.diag
     return the_D
 
 def get_coherent(the_D):
     coherent_max=float("-inf")
-    for i in the_D.shape[0]:
-        for j in the_D.shape[0]:
+    for i in range(the_D.shape[0]):
+        for j in range(the_D.shape[0]):
             if i==j:
                 continue
             temp_coherent_max=(the_D[:,i]*the_D[:,j]).sum()
